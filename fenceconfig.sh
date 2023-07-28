3436aae426fed4c61d0e4322f6bf9e0ccc588946
spalnatik <139609488+spalnatik@users.noreply.github.com>
2023-07-28 11:38:28 +0530

    Update fenceconfig.sh

--- fenceconfig.sh
+++ fenceconfig.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash
 sudo pcs property set stonith-timeout=900
 sudo pcs stonith create rsc_st_azure fence_azure_arm msi=true resourceGroup="$rgname" subscriptionId="$subscriptionID" power_timeout=240 pcmk_reboot_timeout=900 pcmk_monitor_timeout=120 pcmk_monitor_retries=4 pcmk_action_limit=3 pcmk_delay_max=15 op monitor interval=3600
 sudo pcs property set stonith-enabled=true
