5a7b54789074554b9b3e40c430b9fd4c7b080fad
spalnatik <139609488+spalnatik@users.noreply.github.com>
2023-07-28 09:53:57 +0530

    Create fenceconfig.sh

---
+++ fenceconfig.sh
@@ -0,0 +1,1 @@
+sudo pcs stonith create rsc_st_azure fence_azure_arm msi=true resourceGroup="$rgname" subscriptionId="$subscriptionID" power_timeout=240 pcmk_reboot_timeout=900 pcmk_monitor_timeout=120 pcmk_monitor_retries=4 pcmk_action_limit=3 pcmk_delay_max=15 op monitor interval=3600
