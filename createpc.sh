fa2db03d0cb67aba87178a6d0b04f497c53cb2b7
spalnatik <139609488+spalnatik@users.noreply.github.com>
2023-07-30 14:38:31 +0530

    Update createpc.sh

--- createpc.sh
+++ createpc.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 #Create Pacemaker cluster
-decoded_password=$(echo -n "$PASSWORD" | base64 -d)
-
-sudo echo "$decoded_password" |pcs host auth prod-cl1-0 prod-cl1-1 -u hacluster
+sudo echo "abc@12345678" |pcs host auth prod-cl1-0 prod-cl1-1 -u hacluster
 sudo pcs cluster setup nw1-azr prod-cl1-0 prod-cl1-1 totem token=30000
 sudo pcs cluster start --all
 
