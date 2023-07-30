83c61392e6cbbc8da2e27b0c0ac7938821bf670e
spalnatik <139609488+spalnatik@users.noreply.github.com>
2023-07-30 14:39:32 +0530

    Update pc7.sh

--- pc7.sh
+++ pc7.sh
@@ -2,7 +2,7 @@
 
 #Create Pacemaker cluster
 
-sudo echo $password | pcs cluster auth prod-cl1-0 prod-cl1-1 -u hacluster
+sudo echo "abc@12345678" | pcs cluster auth prod-cl1-0 prod-cl1-1 -u hacluster
 sudo pcs cluster setup --name nw1-azr prod-cl1-0 prod-cl1-1 --token 30000
 sudo pcs cluster start --all
 
