36aab15ee02711aae64fefca0eb983132ce1f93a
spalnatik <139609488+spalnatik@users.noreply.github.com>
2023-11-02 20:52:24 +0530

    Delete fencetest.sh

--- fencetest.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rgname=$1
-subscriptionID=$2
-sudo pcs property set stonith-timeout=900
-
-pcs stonith create rsc_st_azure fence_azure_arm msi=true resourceGroup="$rgname" subscriptionId="$subscriptionID" power_timeout=240 pcmk_reboot_timeout=900 pcmk_monitor_timeout=120 pcmk_monitor_retries=4 pcmk_action_limit=3 op monitor interval=3600       
-
-sudo pcs property set stonith-enabled=true
