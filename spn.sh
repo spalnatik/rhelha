5bb74acbdaaa14b9f3a14a388b33ff8f5ff80ac7
spalnatik <139609488+spalnatik@users.noreply.github.com>
2024-07-01 10:21:32 +0530

    Update spn.sh

--- spn.sh
+++ spn.sh
@@ -21,9 +21,9 @@ target_version="2.0.4-6"
 # Compare the pacemaker version and target version
 if [[ "$pacemaker_version" > "$target_version" ]]; then
     # Run following command if you are setting up fence agent on (two-node cluster and pacemaker version greater than 2.0.4-6.el8) OR (HANA scale out)
-    sudo pcs stonith create rsc_st_azure fence_azure_arm username="$clientid" password="$clientsecret" resourceGroup="$rgname" tenantId="72f988bf-86f1-41af-91ab-2d7cd011db47" subscriptionId="$subscriptionID" power_timeout=240 pcmk_reboot_timeout=900 pcmk_monitor_timeout=120 pcmk_monitor_retries=4 pcmk_action_limit=3 op monitor interval=3600       
+    sudo pcs stonith create rsc_st_azure fence_azure_arm username="$clientid" password="$clientsecret" resourceGroup="$rgname" tenantId="16b3c013-d300-468d-ac64-7eda0820b6d3" subscriptionId="$subscriptionID" power_timeout=240 pcmk_reboot_timeout=900 pcmk_monitor_timeout=120 pcmk_monitor_retries=4 pcmk_action_limit=3 op monitor interval=3600       
 else
-    sudo pcs stonith create rsc_st_azure fence_azure_arm username="$clientid" password="$clientsecret" resourceGroup="$rgname" tenantId="72f988bf-86f1-41af-91ab-2d7cd011db47" subscriptionId="$subscriptionID" power_timeout=240 pcmk_reboot_timeout=900 pcmk_monitor_timeout=120 pcmk_monitor_retries=4 pcmk_action_limit=3 pcmk_delay_max=15 op monitor interval=3600
+    sudo pcs stonith create rsc_st_azure fence_azure_arm username="$clientid" password="$clientsecret" resourceGroup="$rgname" tenantId="16b3c013-d300-468d-ac64-7eda0820b6d3" subscriptionId="$subscriptionID" power_timeout=240 pcmk_reboot_timeout=900 pcmk_monitor_timeout=120 pcmk_monitor_retries=4 pcmk_action_limit=3 pcmk_delay_max=15 op monitor interval=3600
 fi
 
 
